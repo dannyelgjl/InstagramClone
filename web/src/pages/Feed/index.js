edbd34c3af3950343ea4b98879559d99b6599da7
dannyelgjl <dannyelgjl@gmail.com>
2020-04-08 16:24:27 -0300

    página do feed estilizada

--- web/src/pages/Feed/index.js
+++ web/src/pages/Feed/index.js
@@ -1,11 +1,73 @@
 import React, { Component } from "react";
 
+import "./Feed.css";
+import more from "../../assets/more.svg";
+import like from "../../assets/like.svg";
+import comment from "../../assets/comment.svg";
+import send from "../../assets/send.svg";
+
 class Feed extends Component {
   render() {
     return (
-      <div>
-        <h1>de boas</h1>
-      </div>
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Daniel Jerônimo</span>
+              <span className="place">João Pessoa</span>
+            </div>
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img
+            src="http://localhost:3333/files/7faf06a3b87723f9019d512a86cb3b66.jpg"
+            alt=""
+          />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+
+            <strong>1000 Curtidas</strong>
+            <p>
+              Show de bola!
+              <span>#javascript #nodejs</span>
+            </p>
+          </footer>
+        </article>
+
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Daniel Jerônimo</span>
+              <span className="place">João Pessoa</span>
+            </div>
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img
+            src="http://localhost:3333/files/7faf06a3b87723f9019d512a86cb3b66.jpg"
+            alt=""
+          />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+
+            <strong>1000 Curtidas</strong>
+            <p>
+              Show de bola!
+              <span>#javascript #nodejs</span>
+            </p>
+          </footer>
+        </article>
+      </section>
     );
   }
 }
