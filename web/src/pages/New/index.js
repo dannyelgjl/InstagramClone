996ca6e5fe968ec392e66851a7dfe19b4e3be5e2
dannyelgjl <dannyelgjl@gmail.com>
2020-04-08 17:08:44 -0300

    página New estilizada e conectada com o back-end

--- web/src/pages/New/index.js
+++ web/src/pages/New/index.js
@@ -1,11 +1,78 @@
 import React, { Component } from "react";
+import api from "../../services/api";
+
+import "./New.css";
 
 class Feed extends Component {
+  state = {
+    image: null,
+    author: "",
+    place: "",
+    description: "",
+    hashtags: "",
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+
+    data.append("image", this.state.image);
+    data.append("author", this.state.author);
+    data.append("place", this.state.place);
+    data.append("description", this.state.description);
+    data.append("hashtags", this.state.hashtags);
+
+    await api.post("posts", data);
+
+    this.props.history.push("/");
+  };
+
+  handleImageChange = (e) => {
+    this.setState({ image: e.target.files[0] });
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
-      <div>
-        <h1>teste</h1>
-      </div>
+      <form id="new-post" onSubmit={this.handleSubmit}>
+        <input type="file" onChange={this.handleImageChange} />
+        <input
+          type="text"
+          name="author"
+          placeholder="Autor do post"
+          value={this.state.autho}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="place"
+          placeholder="Local do post"
+          value={this.place}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="description"
+          placeholder="Descrição do post"
+          value={this.description}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="hashtags"
+          placeholder="Hashtags do post"
+          value={this.hashtags}
+          onChange={this.handleChange}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
