0bc7329887ecdc662b9c731b444373ecfed833c6
dannyelgjl <dannyelgjl@gmail.com>
2020-04-08 16:45:10 -0300

    Feed conectado com o back-end

--- web/src/pages/Feed/index.js
+++ web/src/pages/Feed/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import api from "../../services/api";
+
 import "./Feed.css";
 import more from "../../assets/more.svg";
 import like from "../../assets/like.svg";
@@ -7,66 +9,46 @@ import comment from "../../assets/comment.svg";
 import send from "../../assets/send.svg";
 
 class Feed extends Component {
+  state = {
+    feed: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get("posts");
+
+    this.setState({ feed: response.data });
+  }
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Daniel Jerônimo</span>
-              <span className="place">João Pessoa</span>
-            </div>
-            <img src={more} alt="Mais" />
-          </header>
+        {this.state.feed.map((post) => (
+          <article>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="Mais" />
+            </header>
 
-          <img
-            src="http://localhost:3333/files/7faf06a3b87723f9019d512a86cb3b66.jpg"
-            alt=""
-          />
+            <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="" />
-              <img src={comment} alt="" />
-              <img src={send} alt="" />
-            </div>
+            <footer>
+              <div className="actions">
+                <img src={like} alt="" />
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
 
-            <strong>1000 Curtidas</strong>
-            <p>
-              Show de bola!
-              <span>#javascript #nodejs</span>
-            </p>
-          </footer>
-        </article>
-
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Daniel Jerônimo</span>
-              <span className="place">João Pessoa</span>
-            </div>
-            <img src={more} alt="Mais" />
-          </header>
-
-          <img
-            src="http://localhost:3333/files/7faf06a3b87723f9019d512a86cb3b66.jpg"
-            alt=""
-          />
-
-          <footer>
-            <div className="actions">
-              <img src={like} alt="" />
-              <img src={comment} alt="" />
-              <img src={send} alt="" />
-            </div>
-
-            <strong>1000 Curtidas</strong>
-            <p>
-              Show de bola!
-              <span>#javascript #nodejs</span>
-            </p>
-          </footer>
-        </article>
+              <strong>{post.likes}</strong>
+              <p>
+                {post.descroption}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
