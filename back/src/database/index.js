89d0301a2d896ce3c1cfabb7e7dcd3ac76479e18
dannyelgjl <dannyelgjl@gmail.com>
2020-04-08 11:49:07 -0300

    conexão com o bd

---
+++ back/src/database/index.js
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose";
+
+class Database {
+  constructor() {
+    this.mongo();
+  }
+
+  mongo() {
+    this.connection = mongoose.connect("mongodb://localhost:27017/instagram", {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+}
+
+export default new Database();
